3b26064215f3fe0bb3d81bf5552ba461844b229d
phamducpho1 <phamducpho14t1@gmail.com>
2018-01-10 09:38:48 +0700

    search employer jobs

--- db/migrate/20180102055817_create_templates.rb
+++ db/migrate/20180102055817_create_templates.rb
@@ -4,7 +4,7 @@ class CreateTemplates < ActiveRecord::Migration[5.1]
       t.text :template_body
       t.string :name
       t.references :user, foreign_key: true
-      t.integer :type_of, default: 0
+      t.integer :type_of, default: 1
       t.timestamps
     end
   end
